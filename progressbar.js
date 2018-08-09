0df2ffa8ca4c3fa159955a59c814b9c98ccfe04e
NirOnE <nirone@gmail.com>
2018-08-09 11:40:14 -0400

    Init du projet
    Premier commit des fichiers

---
+++ progressbar.js
@@ -0,0 +1,8 @@
+/*Animation for the progress bar */
+jQuery(document).ready(function () {
+  jQuery('.skillbar').each(function () {
+    jQuery(this).find('.skillbar-bar').animate({
+      width: jQuery(this).attr('data-percent')
+    }, 2000);
+  });
+});
